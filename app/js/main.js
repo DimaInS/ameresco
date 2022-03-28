5de468f2bce9f4d71115ba74eaf0ef61dfb8e51a
DimaLnS <dmadamchuk@gmail.com>
2022-03-28 19:53:55 +0300

    added description

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,12 @@
 $(function() {
+   $('.descr-card__input').styler();
+
   $('.top-slider__items').slick({
     arrows: false,
     dots: true,
     fade: true,
     appendDots: $('.reviws__dots')
   })
+
+
 });
