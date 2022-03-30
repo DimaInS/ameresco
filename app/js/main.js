d0fb78abcc00165d822a6266faaa71386ad3163a
DimaLnS <dmadamchuk@gmail.com>
2022-03-30 20:38:26 +0300

    point 30.03

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,19 @@
 $(function() {
+
     $('.products-popular__btn').on('click', function(){
-      $('.products-popular__btn').removeClass('products-popular__btn--active')
-      $(this).addClass('products-popular__btn--active')
+          $('.products-popular__btn').removeClass('products-popular__btn--active');
+          $(this).addClass('products-popular__btn--active');
+        });
+
+
+    $('.products-popular__btn-list').on('click', function(){
+      $('.products-popular__bottom').addClass('products-popular__bottom--nogrid');
+      $('.products-popular__item').addClass('products-popular__item--list');
+    });
+
+    $('.products-popular__btn-grid').on('click', function(){
+      $('.products-popular__bottom').removeClass('products-popular__bottom--nogrid');
+      $('.products-popular__item').removeClass('products-popular__item--list');
     });
 
    $('.descr-card__input').styler();
